4bff35571faee9aa8eee777a01a8f0aa66abe2b4
Gustavo Pinto <mail@gustavopinto.org>
2024-06-06 19:56:45 -0300

    com instrucoes de prompt do llama

--- hf.py
+++ hf.py
@@ -1,9 +1,24 @@
 from langchain_community.llms import HuggingFaceEndpoint
 import os
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "sua_chave_aqui"
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 model = HuggingFaceEndpoint(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", task="text-generation")
 
-response = model.invoke("Hello World!")
+
+prompt = """
+<|begin_of_text|>
+
+<|start_header_id|>system<|end_header_id|>
+Quero que você atue como um cozinheiro especializado em comida Paraense.
+<|eot_id|>
+
+<|start_header_id|>user<|end_header_id|>
+Como eu posso fazer uma lasanha com açai.
+<|eot_id|>
+
+<|start_header_id|>assistant<|end_header_id|>
+"""
+
+response = model.invoke(prompt)
 print(response)
