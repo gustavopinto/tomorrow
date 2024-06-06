b6b4c2155f3fadaaf512adfd043ed6dc47f173c7
Gustavo Pinto <mail@gustavopinto.org>
2024-06-06 20:18:56 -0300

    agora com laco que interage com o usuario

--- hf.py
+++ hf.py
@@ -6,19 +6,25 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 model = HuggingFaceEndpoint(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", task="text-generation")
 
 
-prompt = """
-<|begin_of_text|>
+while True:
+    user_message = input("> ")
 
-<|start_header_id|>system<|end_header_id|>
-Quero que você atue como um cozinheiro especializado em comida Paraense.
-<|eot_id|>
+    prompt = f"""
+    <|begin_of_text|>
 
-<|start_header_id|>user<|end_header_id|>
-Como eu posso fazer uma lasanha com açai.
-<|eot_id|>
+    <|start_header_id|>system<|end_header_id|>
+    Quero que você atue como um cozinheiro especializado em comida Paraense. 
+    Responda em no maximo 30 palavaras.
+    <|eot_id|>
 
-<|start_header_id|>assistant<|end_header_id|>
-"""
+    <|start_header_id|>user<|end_header_id|>
+    {user_message}
+    <|eot_id|>
 
-response = model.invoke(prompt)
-print(response)
+    <|start_header_id|>assistant<|end_header_id|>
+    """
+
+    response = model.invoke(prompt)
+    print(f"> {response}")
+    print("\n")
+
