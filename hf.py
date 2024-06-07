93bffc9cdfb91364b2ed458fb6050695cdaa1b82
Gustavo Pinto <mail@gustavopinto.org>
2024-06-06 21:10:01 -0300

    com historico de mensagem de usuarios

--- hf.py
+++ hf.py
@@ -3,11 +3,21 @@ import os
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
-model = HuggingFaceEndpoint(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", task="text-generation")
+model = HuggingFaceEndpoint(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", task="text-generation", temperature=0.9)
 
+r = model.invoke("""<|begin_of_text|>
+
+    <|start_header_id|>system<|end_header_id|>
+    me conte uma piada sobre o tacaca.
+    <|eot_id|>
+             
+    <|start_header_id|>assistant<|end_header_id|>""")
+
+history = []
 
 while True:
     user_message = input("> ")
+    history.append(user_message)
 
     prompt = f"""
     <|begin_of_text|>
@@ -21,10 +31,16 @@ while True:
     {user_message}
     <|eot_id|>
 
+    <|start_header_id|>system<|end_header_id|>
+    Para responder, considere o histórico recente de conversa: `{history}`.
+    <|eot_id|>
+
     <|start_header_id|>assistant<|end_header_id|>
     """
 
     response = model.invoke(prompt)
+    history.append(response)
+
     print(f"> {response}")
     print("\n")
 
